3fb9cdab0e02aa351f0a5e9ddaa454ed9aa745a6
Matheus Fontanetti <matheusfontanetti@hotmail.com>
2013-04-04 17:21:18 -0300

    inicio

---
+++ src/br/unesp/igce/siscursos/lacunas/GeradorDisciplinas.java
@@ -0,0 +1,41 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.unesp.igce.siscursos.lacunas;
+import java.util.Random;
+/**
+ *
+ * @author aluno
+ */
+public class GeradorDisciplinas {
+    static private String[] disciplinas1 ={
+        "Microprocessadores ",
+        "Arquitetura",
+        "Sistemas",
+        "Teoria",
+        "Inteligencia",
+        "Linguagens",
+        "Estrutura",
+        
+    };
+    static private String[] disciplinas2 ={
+      "II",
+      "Computadores",
+      "Operacionas",
+      "Orientados",
+      "Computação",
+      "Artificial",
+      "Comercias",
+      "Dados",    
+    };
+    static private String[] disciplinas3 ={
+      "Objetos",
+      "I",
+      "da",
+      "de",
+      "a",
+      "Programação",
+     
+    };
+}
