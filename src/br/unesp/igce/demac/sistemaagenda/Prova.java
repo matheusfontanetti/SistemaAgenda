1bcdac7be0806962bdaa0967c991ded3de8175db
Matheus Fontanetti <matheusfontanetti@hotmail.com>
2013-04-04 17:00:47 -0300

    inicio

---
+++ src/br/unesp/igce/demac/sistemaagenda/Prova.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.unesp.igce.demac.sistemaagenda;
+/**
+ *
+ * @author aluno
+ */
+public class Prova {
+    
+
+
+   private String disciplina;
+   private String docente;
+    private String assunto;
+   
+    Prova(String disciplina,String docente,String assunto){
+        this.disciplina = "Digite o nome da Disciplina :";
+        this.docente ="Digite o nome do docente :";
+        this.assunto="Digite a ementa dada na disciplina :";
+        
+        
+        
+        
+    }  
+    
+   
+    
+    
+}
