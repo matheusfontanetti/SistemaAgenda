975cb42b60758af5ec7e29c8b23c0a61bc52e60f
Matheus Augusto Fontanetti <matheusfontanetti@hotmail.com>
2013-04-17 20:17:11 -0300

    inicio

--- src/br/unesp/igce/siscursos/lacunas/GeradorDisciplinas.java
+++ src/br/unesp/igce/siscursos/lacunas/GeradorDisciplinas.java
@@ -8,8 +8,14 @@ import java.util.Random;
  *
  * @author aluno
  */
+
+
+
+
 public class GeradorDisciplinas {
-    static private String[] disciplinas1 ={
+
+   
+    static public String[] disciplinas1 ={
         "Microprocessadores ",
         "Arquitetura",
         "Sistemas",
@@ -32,10 +38,109 @@ public class GeradorDisciplinas {
     static private String[] disciplinas3 ={
       "Objetos",
       "I",
-      "da",
-      "de",
       "a",
       "Programação",
      
     };
+    
+     static private String disciplinas3() {
+        
+        ExpressaoGeradorDisciplinas disciplinaGerada = new ExpressaoGeradorDisciplinas(15);
+        disciplinaGerada.colar(new ParteSorteadaGeradorDisciplinas(disciplinas3));
+        
+        ExpressaoGeradorDisciplinas disciplinaGerada2 = new ExpressaoGeradorDisciplinas(20);
+        disciplinaGerada2.colar(new ParteFixaGeradorDisciplinas("de "));
+        disciplinaGerada2.colar(new ParteSorteadaGeradorDisciplinas(disciplinas3));
+        disciplinaGerada2.colar(new ParteFixaGeradorDisciplinas(" "));
+        disciplinaGerada2.colar(new ParteSorteadaGeradorDisciplinas(disciplinas3));
+        
+        ExpressaoGeradorDisciplinas disciplinaGerada3 = new ExpressaoGeradorDisciplinas(20);
+        disciplinaGerada3.colar(new ParteSorteadaGeradorDisciplinas(disciplinas3));
+        disciplinaGerada3.colar(new ParteFixaGeradorDisciplinas(" da "));
+        disciplinaGerada3.colar(new ParteSorteadaGeradorDisciplinas(disciplinas3));
+        
+        ExpressaoGeradorDisciplinas disciplinaGerada4 = new ExpressaoGeradorDisciplinas(45);
+        disciplinaGerada4.colar(new ParteSorteadaGeradorDisciplinas(disciplinas3));
+        disciplinaGerada4.colar(new ParteFixaGeradorDisciplinas(" "));
+        disciplinaGerada4.colar(new ParteSorteadaGeradorDisciplinas(disciplinas3));
+        
+        ExpressaoGeradorDisciplinas[] opts = {disciplinaGerada, disciplinaGerada2, disciplinaGerada3, disciplinaGerada4};
+        Gerador g = new Gerador(opts);
+        
+        return g.prox();
+
+    }
+    
+    static public String disciplinas1() {
+        
+        ExpressaoGeradorDisciplinas disciplinasAlgumas = new ExpressaoGeradorDisciplinas(5);
+       
+        disciplinasAlgumas.colar(new ParteSorteadaGeradorDisciplinas(disciplinas1));
+        disciplinasAlgumas.colar(new ParteFixaGeradorDisciplinas(" " + disciplinas2()));
+        
+        ExpressaoGeradorDisciplinas disciplinasAlgumas2 = new ExpressaoGeradorDisciplinas(5);
+        
+        disciplinasAlgumas2.colar(new ParteSorteadaGeradorDisciplinas(disciplinas1));
+        disciplinasAlgumas2.colar(new ParteFixaGeradorDisciplinas(" " + disciplinas2()));
+
+        ExpressaoGeradorDisciplinas disciplinasAlgumas3 = new ExpressaoGeradorDisciplinas(90);
+        disciplinasAlgumas3.colar(new ParteSorteadaGeradorDisciplinas(disciplinas1));
+        disciplinasAlgumas3.colar(new ParteFixaGeradorDisciplinas(" " + disciplinas2()));
+        
+        ExpressaoGeradorDisciplinas[] opts = {disciplinasAlgumas, disciplinasAlgumas2, disciplinasAlgumas3};
+        Gerador g1 = new Gerador(opts);
+        
+       
+        
+        return g1.prox();
+    }
+    
+    static public String disciplinas2() {
+        
+        ExpressaoGeradorDisciplinas disciplinasCriadas = new ExpressaoGeradorDisciplinas(5);
+       
+        disciplinasCriadas.colar(new ParteSorteadaGeradorDisciplinas(disciplinas2));
+        disciplinasCriadas.colar(new ParteFixaGeradorDisciplinas(" " + disciplinas3()));
+        
+        ExpressaoGeradorDisciplinas disciplinasCriadas2 = new ExpressaoGeradorDisciplinas(5);
+        
+        disciplinasCriadas2.colar(new ParteSorteadaGeradorDisciplinas(disciplinas2));
+        disciplinasCriadas2.colar(new ParteFixaGeradorDisciplinas(" " + disciplinas3()));
+
+        ExpressaoGeradorDisciplinas disciplinasCriadas3 = new ExpressaoGeradorDisciplinas(90);
+        disciplinasCriadas3.colar(new ParteSorteadaGeradorDisciplinas(disciplinas2));
+        disciplinasCriadas3.colar(new ParteFixaGeradorDisciplinas(" " + disciplinas3()));
+        
+        ExpressaoGeradorDisciplinas[] opts = {disciplinasCriadas, disciplinasCriadas2, disciplinasCriadas3};
+        Gerador g = new Gerador(opts);
+        return g.prox();
+    }
+   
+    static public String prox() {
+        Random g = new Random();
+        if (g.nextBoolean()) {
+            return disciplinas2();
+        } else {
+            return disciplinas1();
+        }
+    
+    
+    
+    
+    
+    
+    
+  
+   
+        
+    
+    }
+    
+
+    
+    
+    
+    
+    
+    
 }
