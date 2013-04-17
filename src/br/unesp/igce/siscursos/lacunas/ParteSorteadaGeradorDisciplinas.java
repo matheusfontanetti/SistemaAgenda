28ea6def98f02af2b98babca0034091e7b9a5052
Matheus Augusto Fontanetti <matheusfontanetti@hotmail.com>
2013-04-17 20:23:54 -0300

    inicio

--- src/br/unesp/igce/siscursos/lacunas/ParteSorteadaGeradorDisciplinas.java
+++ src/br/unesp/igce/siscursos/lacunas/ParteSorteadaGeradorDisciplinas.java
@@ -3,11 +3,25 @@
  * and open the template in the editor.
  */
 package br.unesp.igce.siscursos.lacunas;
-
+import java.util.Random;
 /**
  *
  * @author WinXP
  */
-public class ParteSorteadaGeradorDisciplinas {
+public class ParteSorteadaGeradorDisciplinas implements ParteExpressaoGeradorDisciplinas {
+    
+  private String[] roleta;
+    
+    public ParteSorteadaGeradorDisciplinas(String[] valores) {
+        roleta = valores;
+    }
+    
+    @Override
+    public String get(){
+        Random g = new Random();
+        int sorteio = g.nextInt(roleta.length);
+        return roleta[sorteio];
+    }  
+    
     
 }
