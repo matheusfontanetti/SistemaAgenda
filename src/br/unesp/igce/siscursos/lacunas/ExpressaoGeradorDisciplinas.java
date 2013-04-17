09c3f55e18f7266b06aef2330da1e3ddf8458ec0
Matheus Augusto Fontanetti <matheusfontanetti@hotmail.com>
2013-04-17 20:21:04 -0300

    inicio

--- src/br/unesp/igce/siscursos/lacunas/ExpressaoGeradorDisciplinas.java
+++ src/br/unesp/igce/siscursos/lacunas/ExpressaoGeradorDisciplinas.java
@@ -3,11 +3,34 @@
  * and open the template in the editor.
  */
 package br.unesp.igce.siscursos.lacunas;
-
+import java.util.ArrayList;
 /**
  *
  * @author WinXP
  */
 public class ExpressaoGeradorDisciplinas {
+    public int peso;
+    private ArrayList<ParteExpressaoGeradorDisciplinas> partes = new ArrayList<ParteExpressaoGeradorDisciplinas>();
     
-}
+    public ExpressaoGeradorDisciplinas(int peso){
+        this.peso = peso;
+    }
+    public void colar(ParteExpressaoGeradorDisciplinas parte) {
+     partes.add(parte);    
+    } 
+    
+    public ParteExpressaoGeradorDisciplinas[] toArray(){
+        Object [] ar  = partes.toArray();
+     ParteExpressaoGeradorDisciplinas[] res = new ParteExpressaoGeradorDisciplinas[ar.length];
+            for (int i = 0; i < res.length; i++) {
+                res[i] =(ParteExpressaoGeradorDisciplinas) ar[i];
+                
+            }
+         return res;
+    }
+    
+    }
+    
+    
+    
+
